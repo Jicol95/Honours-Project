35329cbe40d9032e41cf15407746cf76369cd1a3
jack <jack.nicol95@gmail.com>
2018-02-09 00:02:07 +0000

    Quick work around for show() function

--- Analyse.py
+++ Analyse.py
@@ -1,4 +1,5 @@
-from music21 import stream
+from music21 import stream, metadata
+from Score import Score
 
 
 class Analyse:
@@ -16,3 +17,16 @@ class Analyse:
                 listindex.append(i)
                 i = string.find(sub_string, i + 1)
             return len(sub_string), listindex
+
+    def Search_Score(self, filename, sequence):
+        parsed_score = Score(filename)
+        pattern_engine = Analyse()
+        voice_notes = parsed_score.voice_notes
+        seq_len, match_index = pattern_engine.find_sequence(parsed_score, sequence)
+        print(match_index)
+        for i in range(0, len(match_index)):
+            pattern = voice_notes[0][match_index[i]: match_index[i] + seq_len + 1]
+            pattern.insert(0, metadata.Metadata())
+            pattern.metadata.title = 'Sequence Match'
+            pattern.metadata.composer = 'Github: https://github.com/Jicol95'
+            pattern.show('text')
--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from Score import Score
 from Analyse import Analyse
-from Menu import Menu
 import argparse
 from music21 import stream
 
 
 def Main():
-    parsed_score = Score('bwv0254')
     pattern_engine = Analyse()
-    # parsed_score.voice_notes[0].show()
-    # Speghet codez should really be in a class somewhere
-    sequence_len, pattern_index = pattern_engine.find_sequence(parsed_score, '+0+0')
-    stre = (parsed_score.voice_notes[0][pattern_index[0]:pattern_index[0] + sequence_len + 1])
-    stre.show()
+    pattern_engine.Search_Score('bwv0254', '+0+0')
 
 
 if __name__ == '__main__':
